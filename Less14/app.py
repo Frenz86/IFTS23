e5b919de60aa20c8c668a514035912cb5ec5b287
Frenz86 <frenzy86@gmail.com>
2023-05-17 12:58:17 +0200

    ww

--- Less14/app.py
+++ Less14/app.py
@@ -1,25 +1,40 @@
-import plotly.express as px
+# importo tutte le librerie necessarie
 import streamlit as st
+import pandas as pd
+import toml
 from datetime import datetime, date
+import matplotlib.pyplot as plt
 from meteostat import Point, Daily
+import pandas as pd
+import plotly.graph_objects as go
+from geopy.geocoders import Nominatim
 
+
+# implemento la funzione main
 def main():
-    st.subheader('title')
-    start_ = st.date_input(
-                            "Start date",
-                            date(2019, 7, 6))
 
-    start = datetime(start_.year, start_.month, start_.day)
+    # file per settare colori di sfondo e testo
+    with open('.streamlit/config.toml', 'r') as f:
+        config = toml.load(f)
 
+    st.header("Weather")
 
-    end_ = st.date_input(
-                        "End date",
-                        date(2023, 5, 17))
-    
-    end = datetime(end_.year, end_.month, end_.day)
+    citta = st.text_input("Inserisci il nome della città", value="Bologna")
 
-    cities = {'Bologna':[44.498955,11.327591]}
+    geolocator = Nominatim(user_agent="Your_name")
+    location = geolocator.geocode(citta)
 
+    # lat = st.number_input("Inserisci la latitudine", value=44.498955, format="%6f")
+    # long = st.number_input("Inserisci la longitudine", value=11.327591, format="%6f")
+
+    data_inizio = st.date_input("Inserisci la data di inizio", date(2019,7,6))
+
+    data_fine = st.date_input("Inserisci la data di fine", date(2023,5,17))
+
+    start = datetime(data_inizio.year, data_inizio.month, data_inizio.day)
+    end = datetime(data_fine.year, data_fine.month, data_fine.day)
+
+    cities = {citta:[location.latitude,location.longitude]}
 
     # Create Point for Vancouver, BC
     city = Point(list(cities.values())[0][0],list(cities.values())[0][1], 20)
@@ -29,9 +44,6 @@ def main():
     df = df.fetch()
     df['city'] = list(cities.keys())[0]
 
-    ### ZOOM 
-
-    import plotly.graph_objects as go
     fig = go.Figure()
 
     #Actual 
@@ -74,6 +86,10 @@ def main():
     fig.add_vline(x=date.today(), line_width=3, line_dash="dash", line_color="red")
     fig.update_layout(width=850)
     st.plotly_chart(fig)
+    st.map(pd.DataFrame({'lat' : [location.latitude] , 'lon' : [location.longitude]},columns = ['lat','lon']))
 
+    
+
+# questo modulo sarà eseguito solo se runnato
 if __name__ == "__main__":
     main()
